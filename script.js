8ed67cf2c406edd4b7a0492c8a7e1066e7b99db9
Evelin24 <128430000+Evelin24@users.noreply.github.com>
2025-05-22 14:00:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -28,21 +28,21 @@ const locations = [
     id: 'itacare',
     coords: [-14.2794, -38.9957],
     title: "Rua Pedro Longo - Itacaré",
-    images: ['imagens/imagemitacare1.jpg', 'imagens/imagemitacare2.jpg', 'imagens/imagemitacare3.jpg'],
+    images: ['uimagens/imagemitacare1.jpg', 'uimagens/imagemitacare2.jpg', 'uimagens/imagemitacare3.jpg'],
     desc: 'A Rua Pedro Longo, mais conhecida como Rua da Pituba, é um dos pontos mais vibrantes de Itacaré. Durante o dia, é ideal para passeios tranquilos, com lojas de artesanato, cafeterias e restaurantes aconchegantes. À noite, se transforma em um espaço animado, com música ao vivo, turistas e moradores curtindo a vibe local. O clima é acolhedor e cheio de vida, com muitas cores, natureza e uma energia que representa bem o espírito de Itacaré. É parada obrigatória para quem quer sentir o ritmo da cidade e aproveitar o melhor da cultura baiana.'
   },
   {
     id: 'itabuna',
     coords: [-14.7876, -39.2781],
     title: "Av. Cinquentenário - Itabuna",
-    images: ['imagens/itabuna1.jpg', 'imagens/itabuna2.jpg', 'imagens/itabuna3.jpg'],
+    images: ['uimagens/itabuna1.jpg', 'uimagens/itabuna2.jpg', 'uimagens/itabuna3.jpg'],
     desc: 'A Lavagem do Beco, festa tradicional em Itabuna realizada no Beco do Fuxico, na Avenida Cinquentenário, combina celebração cultural e religiosa, com influências do candomblé e samba. A festa inclui a lavagem simbólica das ruas com água de cheiro, acompanhada de tambores e cânticos afro-brasileiros, além de apresentações artísticas, rodas de samba e feiras de artesanato. Com o tempo, tornou-se um símbolo de revitalização urbana e resistência cultural, valorizando a memória e a identidade da comunidade local.'
   },
   {
     id: 'sp',
     coords: [-23.5343, -46.6337],
     title: "Pinacoteca de São Paulo",
-    images: ['imagens/pinacoteca1.jpg', 'imagens/pinacoteca2.jpg', 'imagens/pinacoteca3.jpg', 'imagens/pinacoteca4.jpg'],
+    images: ['uimagens/pinacoteca1.jpg', 'uimagens/pinacoteca2.jpg', 'uimagens/pinacoteca3.jpg', 'uimagens/pinacoteca4.jpg'],
     desc: 'A Pinacoteca do Estado de São Paulo, fundada em 1905, é um dos museus de arte mais importantes do Brasil, localizada no centro da cidade. Seu acervo reúne mais de 10 mil obras, desde o período colonial até a arte contemporânea, com destaque para artistas brasileiros como Tarsila do Amaral e Anita Malfatti. Além das exposições permanentes, oferece mostras temporárias, cursos e atividades educativas. O museu, que combina arquitetura clássica e moderna, também possui um jardim de esculturas, sendo um importante centro cultural que valoriza a arte brasileira.'
   }
 ];
